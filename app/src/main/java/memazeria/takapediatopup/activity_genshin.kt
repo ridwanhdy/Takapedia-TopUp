040ee640583c8c79c316db7b3b9530f6052e3d4e
madharizqul76 <madharizkul@gmail.com>
2023-11-20 00:13:56 +0700

    list Katalog 1.0

---
+++ app/src/main/java/memazeria/takapediatopup/activity_genshin.kt
@@ -0,0 +1,11 @@
+package memazeria.takapediatopup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_genshin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_genshin)
+    }
+}
