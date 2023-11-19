db59caa4fcecbe5607726d0334b0673ff073c1d7
ridwanhidayat <ridwan44404@gmail.com>
2023-11-19 20:19:38 +0700

    menambah res/font

--- app/src/main/java/memazeria/takapediatopup/home.kt
+++ app/src/main/java/memazeria/takapediatopup/home.kt
@@ -1,11 +1,21 @@
 package memazeria.takapediatopup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnMlbb = findViewById<Button>(R.id.mlbb)
+
+        btnMlbb.setOnClickListener {
+            Intent(this, activity_ml::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
