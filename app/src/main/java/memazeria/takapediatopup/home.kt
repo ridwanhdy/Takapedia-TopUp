3ad689035f4c62fe0d46dc4b1af4d93bd1f61255
madharizqul76 <madharizkul@gmail.com>
2023-11-20 07:45:44 +0700

    list Katalog 3.0

--- app/src/main/java/memazeria/takapediatopup/home.kt
+++ app/src/main/java/memazeria/takapediatopup/home.kt
@@ -11,9 +11,30 @@ class home : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val btnMlbb = findViewById<Button>(R.id.mlbb)
+        val btnPubg = findViewById<Button>(R.id.pubg)
+        val btnGenshin = findViewById<Button>(R.id.genshin)
+        val btnFreeFire = findViewById<Button>(R.id.ff)
 
         btnMlbb.setOnClickListener {
             Intent(this, activity_ml::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        btnPubg.setOnClickListener {
+            Intent(this, activity_pubg::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        btnGenshin.setOnClickListener {
+            Intent(this, activity_genshin::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        btnFreeFire.setOnClickListener {
+            Intent(this, activity_ff::class.java).also {
                 startActivity(it)
             }
         }
