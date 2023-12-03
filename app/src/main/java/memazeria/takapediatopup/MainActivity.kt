2e8518b5df070e51a036edaa39164903a4097563
ridwanhidayat <ridwan44404@gmail.com>
2023-12-03 18:09:48 +0700

    Update Fragment Profil Pengguna sekarang sudah bisa menampilkan email

--- app/src/main/java/memazeria/takapediatopup/MainActivity.kt
+++ app/src/main/java/memazeria/takapediatopup/MainActivity.kt
@@ -16,5 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
     }
 }
--- app/src/main/java/memazeria/takapediatopup/profile_pengguna.kt
+++ app/src/main/java/memazeria/takapediatopup/profile_pengguna.kt
@@ -1,11 +1,70 @@
 package memazeria.takapediatopup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class profile_pengguna : AppCompatActivity() {
+    private lateinit var textEmail: TextView
+    private lateinit var buttonLogout: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_pengguna)
+
+        // Inisialisasi TextView dan Button
+        textEmail = findViewById(R.id.textEmail)
+        buttonLogout = findViewById(R.id.buttonbantuan)
+
+        // Memanggil fungsi untuk menampilkan email dari Firebase
+        displayEmailFromFirebase()
+
+        // Menambahkan onClickListener pada button logout
+        buttonLogout.setOnClickListener {
+            showLogoutConfirmationDialog()
+        }
+    }
+
+    private fun displayEmailFromFirebase() {
+        // Mendapatkan instance Firebase Auth
+        val auth = FirebaseAuth.getInstance()
+
+        // Memeriksa apakah pengguna sedang login
+        val user = auth.currentUser
+        if (user != null) {
+            // Mendapatkan email pengguna dari objek User
+            val email = user.email
+
+            // Menampilkan email ke dalam TextView
+            textEmail.text = "Email: $email"
+        }
+    }
+
+    private fun showLogoutConfirmationDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Logout")
+        builder.setMessage("Yakin ingin logout?")
+        builder.setPositiveButton("Ya") { _, _ ->
+            // Melakukan logout
+            logout()
+        }
+        builder.setNegativeButton("Tidak", null)
+        builder.show()
+    }
+
+    private fun logout() {
+        // Melakukan logout dari Firebase
+        FirebaseAuth.getInstance().signOut()
+
+        // Pindah ke activity login
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+
+        finish() // Menutup activity saat ini
     }
 }
